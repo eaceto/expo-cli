51fe86ef9ff83d86e641a87c9a5182c9defd2a19
Dominik Sokal <dominiksokal@gmail.com>
2018-10-18 15:25:02 +0200

    wip

--- packages/expo-cli/src/commands/upload.js
+++ packages/expo-cli/src/commands/upload.js
@@ -1,38 +1,68 @@
+import _ from 'lodash';
+
 import IOSUploader from './upload/IOSUploader';
 import AndroidUploader from './upload/AndroidUploader';
 import log from '../log';
 
-function ensurePlatformIsCorrect() {
+const COMMON_OPTIONS = ['id', 'latest', 'path'];
+
+export default program => {
+  const androidCommand = program
+    .command('upload:android [projectDir]')
+    .alias('ua')
+    .option('--key <key>', 'path to the JSON key used to authenticate with the Google Play Store')
+    .description(
+      'Uploads standalone android app to the Google Play Store (it works on macOS only). Uploads the latest build by default.'
+    )
+    .asyncActionProjectDir(createUploadAction(AndroidUploader, [...COMMON_OPTIONS, 'key']));
+  setCommonOptions(androidCommand, '.apk');
+
+  const iosCommand = program
+    .command('upload:ios [projectDir]')
+    .alias('ui')
+    .option(
+      '--apple-id <apple-id>',
+      'your Apple ID username (you can also set EXPO_APPLE_ID env variable)'
+    )
+    .option(
+      '--apple-id-password <apple-id-password>',
+      'your Apple ID password (you can also set EXPO_APPLE_ID_PASSWORD env variable)'
+    )
+    .description(
+      'Uploads standalone app to App Store (it works on macOS only). Uploads the latest build by default.'
+    )
+    .asyncActionProjectDir(createUploadAction(IOSUploader, [...COMMON_OPTIONS, 'appleId']));
+  setCommonOptions(iosCommand, '.ipa');
+};
+
+function setCommonOptions(command, fileExtension) {
+  command
+    .option('--id <id>', 'id of the build to upload')
+    .option('--latest', 'uploads the latest build')
+    .option('--path <path>', `path to the ${fileExtension} file`);
+}
+
+function createUploadAction(UploaderClass, optionKeys) {
+  return async (projectDir, command) => {
+    ensurePlatformIsSupported();
+    await ensureOptionsAreValid(command);
+
+    const options = _.pick(command, optionKeys);
+    const uploader = new UploaderClass(projectDir, options);
+    await uploader.upload();
+  };
+}
+
+function ensurePlatformIsSupported() {
   if (process.platform !== 'darwin') {
-    log.error('Unsupported platform! this feature works on macOS only');
+    log.error('Unsupported platform! This feature works on macOS only.');
     process.exit(1);
   }
 }
 
-export default (program: any) => {
-  program
-    .command('upload:android [projectDir]')
-    .option('--id <id>', 'Upload build with provided id')
-    .option('--latest', 'Upload latest build')
-    .option('--path <path>', 'Upload local build')
-    .option('--key <key>', 'JSON key used to authenticate with Google Play Store')
-    .description('Upload standalone android app to Google Play Store. It works only on macOS')
-    .asyncActionProjectDir(async (projectDir, command) => {
-      ensurePlatformIsCorrect();
-      const uploader = new AndroidUploader(projectDir, command);
-      await uploader.upload();
-    });
-
-  program
-    .command('upload:ios [projectDir]')
-    .option('--id <id>', 'Upload build with provided id')
-    .option('--latest', 'Upload latest build')
-    .option('--path <path>', 'Upload local build')
-    .option('--apple-id <apple-id>', 'Your Apple ID')
-    .description('Uploads standalone app to App Store. It works only on macOS')
-    .asyncActionProjectDir(async (projectDir, command) => {
-      ensurePlatformIsCorrect();
-      const uploader = new IOSUploader(projectDir, command);
-      await uploader.upload();
-    });
-};
+async function ensureOptionsAreValid(command) {
+  const args = _.pick(command, COMMON_OPTIONS);
+  if (_.size(args) > 1) {
+    throw new Error(`You have to choose only one of --path, --id or --latest parameters`);
+  }
+}
--- packages/expo-cli/src/commands/upload/IOSUploader.js
+++ packages/expo-cli/src/commands/upload/IOSUploader.js
@@ -1,54 +1,73 @@
-import prompt from '../../prompt';
-import log from '../../log';
+import _ from 'lodash';
 
 import BaseUploader from './BaseUploader';
 import { printFastlaneError, spawnAndCollectJSONOutputAsync } from './utils';
+import prompt from '../../prompt';
+
+const PLATFORM = 'ios';
+
+const APPLE_CREDS_QUESTIONS = [
+  {
+    type: 'input',
+    name: 'appleId',
+    message: `What's your Apple ID?`,
+  },
+  {
+    type: 'password',
+    name: 'appleIdPassword',
+    message: 'Password?',
+  },
+];
 
 export default class IOSUploader extends BaseUploader {
   constructor(projectDir, options) {
-    const args = {
-      projectDir,
-      options,
-      platform: 'ios',
-      platformName: 'iOS',
-      platformExtension: 'ipa',
-    };
-    super(args);
+    super(PLATFORM, projectDir, options);
   }
 
-  ensureConfigDataIsCorrect(configData) {
-    const { ios } = configData;
-    if (!ios || !ios.bundleIdentifier) {
-      throw new Error(`Must specify a bundle identifier in order to upload ipa file.`);
+  _ensureExperienceIsValid(exp) {
+    if (!_.has(exp, 'ios.bundleIdentifier')) {
+      throw new Error(`You must specify an iOS bundle identifier in app.json.`);
     }
   }
 
-  async getPlatformData() {
-    if (!this.options.appleId) {
-      log('You can specify your Apple ID using --apple-id option');
-      return prompt({
-        name: 'appleId',
-        message: 'Your Apple ID Username: ',
-        type: 'input',
-      });
+  async _getPlatformSpecificOptions() {
+    const result = _.pick(this.options, appleCredsKeys);
+
+    if (process.env.EXPO_APPLE_ID) {
+      result.appleId = process.env.EXPO_APPLE_ID;
     }
-    return { appleId: this.options.appleId };
+    if (process.env.EXPO_APPLE_ID_PASSWORD) {
+      result.appleIdPassword = process.env.EXPO_APPLE_ID_PASSWORD;
+    }
+
+    const appleCredsKeys = ['appleId', 'appleIdPassword'];
+    const credsMissing = _.difference(Object.keys(this.options), appleCredsKeys);
+
+    if (_.size(credsMissing) > 0) {
+      const questions = APPLE_CREDS_QUESTIONS.filter(({ name }) => !credsMissing.includes(name));
+      const answers = await prompt(questions);
+      Object.assign(result, answers);
+    }
+
+    return result;
   }
 
-  async uploadToStore({ name: appName, ios: { bundleIdentifier } }, { appleId }, path) {
-    const fastlane = this.fastlane;
-    const login = await spawnAndCollectJSONOutputAsync(fastlane.app_produce, [
-      bundleIdentifier,
-      appName,
-      appleId,
-    ]);
-    if (login.result !== 'success') {
-      printFastlaneError(login, 'login');
-      return;
-    }
-    const upload = await spawnAndCollectJSONOutputAsync(fastlane.app_deliver, [path, appleId]);
-    if (upload.result !== 'success') {
-      printFastlaneError(upload, 'upload');
-    }
+  async _uploadToTheStore({ appleId }, path) {
+    const { name: appName, ios: { bundleIdentifier } } = this.options;
+    const { fastlane } = this;
+    console.log([bundleIdentifier, appName, appleId]);
+    // const login = await spawnAndCollectJSONOutputAsync(fastlane.app_produce, [
+    //   bundleIdentifier,
+    //   appName,
+    //   appleId,
+    // ]);
+    // if (login.result !== 'success') {
+    //   printFastlaneError(login, 'login');
+    //   return;
+    // }
+    // const upload = await spawnAndCollectJSONOutputAsync(fastlane.app_deliver, [path, appleId]);
+    // if (upload.result !== 'success') {
+    //   printFastlaneError(upload, 'upload');
+    // }
   }
 }
